b5e3072f9096715875989b24744fa39ca96cfe5f
Bailey Long <bailey-long@live.nmit.ac.nz>
2022-05-30 14:54:42 +1200

    code cleaned and error found

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+let obj = null;
+let num = null;
 //ANCHOR Data for the level objects
 let mundane = {
     weapon: ["+1 Dagger ", "Masterwork Kama "]
@@ -12,7 +14,7 @@ function getRandInRange(min, max){
 //ANCHOR Data for random treasure generator stored in objects representing party character levels.
 // level 1 treasure table
 let level1 = {
-    coins: ["No coin", getRandInRange(1,6) * 1000 + " Copper Pieces ", getRandInRange(1,8) * 100 + " Silver Pieces ", getRandInRange(1,4) * 10 + " Platinum Pieces "],
+    coins: ["No coin", getRandInRange(1,6) * 1000 + " Copper Pieces ", getRandInRange(1,8) * 100 + " Silver Pieces ", getRandInRange(1,4) * 10 + " Gold Pieces "],
     goods: ["nothing ", "Extravagant painting ", "ornate statue "],
     items: ["Boots of speed ", mundane.weapon[getRandInRange(0, mundane.weapon.length-1)], "Potion of fire breathing "],
 }
@@ -22,6 +24,13 @@ let level2 = {
     goods: "Big painting",
     items: "+1 longsword",
 }
+//ANCHOR get values from clicking
+function setValue(){
+    var select = document.getElementById('getObj');
+    console.log(obj = select.options[select.selectedIndex].value);
+    console.log(num = document.getElementById('getNum').value);
+    buildHorde(obj,num);
+}
 //ANCHOR treasure generator, obj refers to the party character levels objects.
 function buildHorde(obj,num){
     // "zero out" arrays for a new treasure roll
@@ -30,15 +39,16 @@ function buildHorde(obj,num){
     let goodsArr = [];
     // for every num add a random coin roll
     for (i = 0; i < num; i ++){
-        coinArr.push(obj.coins[getRandInRange(0, obj.coins.length-1)]);
+        coinArr.push(obj.coins[getRandInRange(0, obj[coins].length-1)]);
+        console.log(obj[coins])
     }
     // for every num add a random item roll
     for (i = 0; i < num; i++){
-        itemArr.push(obj.items[getRandInRange(0, obj.items.length-1)]);
+        itemArr.push(obj.items[getRandInRange(0, obj[items].length-1)]);
     }
     // for every num add a random goods roll
     for (i = 0; i < num; i ++){
-        goodsArr.push(obj.goods[getRandInRange(0, obj.goods.length-1)]);
+        goodsArr.push(obj.goods[getRandInRange(0, obj[goods].length-1)]);
     }
     // following lines add the new arrays into the html document.
     document.getElementById("coins").innerHTML = coinArr;
